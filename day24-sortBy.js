fb2f07ad4b09c81d2e766bbf03062eafc0b521c7
farmerlin731 <69831413+farmerlin731@users.noreply.github.com>
2024-05-16 13:09:58 +0800

    day24:sortBy (5/16)

---
+++ day24-sortBy.js
@@ -0,0 +1,13 @@
+const arr = [{ x: 1 }, { x: 0 }, { x: -1 }],
+  fn = (d) => d.x;
+
+// const arr = [5, 4, 1, 2, 3],
+//   fn = (x) => x;
+
+function sortBy(arr, fn) {
+  const result = [...arr];
+  result.sort((a, b) => fn(a) - fn(b));
+  return result;
+}
+
+console.log(sortBy(arr, fn));
