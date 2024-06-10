f307414dc2776dabbfb1969575cb359cbd4f0162
farmerlin731 <69831413+farmerlin731@users.noreply.github.com>
2024-06-10 16:37:39 +0800

    day49:compactObj (6/10)

--- day49-compactObj.js
+++ day49-compactObj.js
@@ -1,1 +1,31 @@
-function compact() {}
+//First Idea:
+//Use recursion and judge the input is Obj/Array/Primitive Value.
+
+function compact(obj) {
+  if (typeof obj !== "object" || obj == null) return obj;
+  let result;
+  if (Array.isArray(obj)) {
+    //Input is an array.
+    for (const item of obj) {
+      if (compact(item)) {
+        result.push(compact(item));
+      }
+    }
+    // result = obj.map((item) => compact(item)).filter(Boolean);
+  } else {
+    //Input is a object.
+    result = {};
+    for (const key in obj) {
+      if (compact(obj[key])) {
+        result[key] = compact(obj[key]);
+      }
+    }
+  }
+
+  return result;
+}
+
+const obj = [null, 0, false, 1];
+// const obj = { a: null, b: [false, 1] };
+
+console.log(compact(obj));
